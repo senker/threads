50d896bc3692bd8bd055d81441462dc399d069b7
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-04 13:41:03 +0100

    chore: add relevant imports

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,3 +1,24 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
+import { z } from 'zod';
+import Image from 'next/image';
+import { ChangeEvent, useState } from 'react';
+
 const AccountProfile = ({ user, btnTitle }: Props) => {
 };
 

62d60d575f181c2614e501a63168c230ff2f975a
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-04 13:42:25 +0100

    chore: component implementation
    Including image handling
    keeping track of the state of the image
    populating the form (ui implementation in next commit)

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -32,6 +32,47 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image || '',
+      name: user?.name || '',
+      username: user?.username || '',
+      bio: user?.bio || '',
+    },
+  });
+
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void,
+  ) => {
+      e.preventDefault;
+
+      const fileReader = new FileReader();
+
+      if (e.target.files && e.target.files.length > 1) {
+          const file = e.target.files[0];
+
+          setFiles(Array.from(e.target.files));
+
+          if (!file.type.includes('image')) return;
+
+          fileReader.onload = async (event) => {
+              const imageDataUrl = event.target?.result?.toString() || '';
+
+              fieldChange(imageDataUrl);
+          }
+
+          fileReader.readAsDataURL(file);
+      }
+  };
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 };
 
 export default AccountProfile;

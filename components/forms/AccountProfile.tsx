05c3874ce6a6a5b530f1e032da6a93adc5c877cc
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-04 13:41:21 +0100

    chore: create props through interface

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,18 @@ import { z } from 'zod';
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
 const AccountProfile = ({ user, btnTitle }: Props) => {
 };
 

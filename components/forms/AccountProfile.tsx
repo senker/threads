112497a06ffeaf46209832cfb528c13cb0701e35
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-05 10:30:45 +0100

    chore: update imports

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,7 +19,9 @@ import { z } from 'zod';
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
-import { useUploadThing } from '@/lib/uploadthing'
+import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Props {
   user: {

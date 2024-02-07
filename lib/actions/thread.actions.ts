19cdabe869542e52cab59a36a9c68753e3f78ca1
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-07 20:09:49 +0100

    fix: using try - catch inside actions

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -14,18 +14,24 @@ interface Params {
 export async function createThread({
     text, author, communityId, path
 }: Params) {
-    connectToDB();
+    try {
+        connectToDB();
 
-    const createdThread = await Thread.create({
-        text,
-        author,
-        community: null,
-    });
+        const createdThread = await Thread.create({
+            text,
+            author,
+            community: null,
+        });
 
-    // Update user model
-    await User.findByIdAndUpdate(author, {
-        $push: { threads: createdThread._id }
-    })
+        // Update user model
+        await User.findByIdAndUpdate(author, {
+            $push: { threads: createdThread._id }
+        })
 
-    revalidatePath(path);
+        revalidatePath(path);
+    } catch (error: any) {
+        throw new Error(`Error creating thread: ${error.message}`)
+    }
+
+
 }

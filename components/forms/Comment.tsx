b90a1225470199ee805f055856e111599b99cd97
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-11 09:59:56 +0100

    feat: comment added to db succesfully

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -19,7 +19,7 @@ import { usePathname, useRouter } from 'next/navigation';
 // import { updateUser } from '@/lib/actions/user.actions';
 import { CommentValidation } from '@/lib/validations/thread';
 import Image from 'next/image';
-// import { createThread } from '@/lib/actions/thread.actions';
+import { addCommentToThread } from '@/lib/actions/thread.actions';
 
 interface Props {
   threadId: string;
@@ -39,14 +39,9 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
   });
 
   const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
-    /* await createThread({
-              text: values.thread,
-              author: userId,
-              communityId: null,
-              path: pathname
-          }); */
+    await addCommentToThread(threadId, values.thread, JSON.parse(currentUserId), pathname);
 
-    router.push('/');
+      form.reset();
   };
 
   return (

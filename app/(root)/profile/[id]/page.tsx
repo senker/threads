3b89e82ab541d92ea3baaf3b2f29ceb2c880f711
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-11 11:34:25 +0100

    chore: prepare groundwork for profile

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,18 @@
+import PostThread from '@/components/forms/PostThread';
+import { fetchUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  return <section>Profile</section>;
+};
+
+export default Page;

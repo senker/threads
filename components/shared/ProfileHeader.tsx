3e92513484bd778eaaabfb0aa5303f2b49eb4a05
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-15 22:50:09 +0100

    chore: formatted shared department

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
   username: string;
   imgUrl: string;
   bio: string;
-  type?: 'User' | 'Community'
+  type?: 'User' | 'Community';
 }
 
 const ProfileHeader = ({
@@ -17,7 +17,7 @@ const ProfileHeader = ({
   username,
   imgUrl,
   bio,
-  type
+  type,
 }: Props) => {
   return (
     <div className="flex w-full flex-col justify-start">

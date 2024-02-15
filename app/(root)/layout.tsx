2f05a4f3c06c1839d65adbf574cfeed5d1f322c1
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2024-02-15 22:51:03 +0100

    chore: formatted threads

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../globals.css";
-import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
-import TopSidebar from "@/components/shared/Topbar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Bottombar from "@/components/shared/Bottombar";
+import TopSidebar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Threads',
-  description: 'A Next.js 13 Meta Threads Application'
-}
+  description: 'A Next.js 13 Meta Threads Application',
+};
 
 export default function RootLayout({
   children,
@@ -29,9 +29,7 @@ export default function RootLayout({
             <LeftSidebar />
 
             <section className="main-container">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+              <div className="w-full max-w-4xl">{children}</div>
             </section>
 
             <RightSidebar />
